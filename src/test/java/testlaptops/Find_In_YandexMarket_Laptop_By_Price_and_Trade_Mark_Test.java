3f7f507f31ef2d66d374ce837999a1e105429f51
yugu0114 <extraponi@gmail.com>
2017-06-24 02:09:39 +0400

    add order for tests

--- src/test/java/testlaptops/Find_In_YandexMarket_Laptop_By_Price_and_Trade_Mark_Test.java
+++ src/test/java/testlaptops/Find_In_YandexMarket_Laptop_By_Price_and_Trade_Mark_Test.java
@@ -2,19 +2,19 @@ package testlaptops;
 
 import org.junit.*;
 import org.junit.rules.ErrorCollector;
+import webbrowser.instances.FirefoxStandaloneBrowser;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import webbrowser.instances.*;
-
 
 /**
  * Created by 1 on 24.06.2017.
  */
+@FixMethodOrder
 public class Find_In_YandexMarket_Laptop_By_Price_and_Trade_Mark_Test {
     private static SearchLaptopInterface testFacade;
 
     @Rule
-    public ErrorCollector collector= new ErrorCollector();
+    public ErrorCollector collector = new ErrorCollector();
 
     @BeforeClass
     public static void beforeClass() {
@@ -23,7 +23,7 @@ public class Find_In_YandexMarket_Laptop_By_Price_and_Trade_Mark_Test {
     }
 
     @Test
-    public void test1(){
+    public void count_of_HP_and_Lenovo_laptops_which_are_cheaper_than_30000_must_be_10(){
         testFacade.getWebBrowser().manage().window().maximize();
         testFacade.getWebBrowser().switchTo();
         testFacade.getWebBrowser().navigate().to("https://yandex.ru");
@@ -35,19 +35,20 @@ public class Find_In_YandexMarket_Laptop_By_Price_and_Trade_Mark_Test {
         testFacade.checkLaptopMarkLenovo();
         testFacade.checkLaptopMarkHP();
         testFacade.pressAcceptFilterButton();
+        int resultRowsCount = testFacade.getCountOfFoundLaptopsSatisfyingTheConditions();
 
-        collector.checkThat("Count of HP and Lenovo laptops which are cheaper than 30000 must be 10.",
-                testFacade.getCountOfFoundLaptopsSatisfyingTheConditions(), // actual
-                equalTo(10));                                                      //expected
+        collector.checkThat("Count of HP and Lenovo laptops which are cheaper than 30000 isn't 10 -> AR: "+ resultRowsCount,
+                resultRowsCount, // actual
+                equalTo(10));  //expected
     }
 
     @Test
-    public void test2(){
+    public void checking_global_searching_works_fine_for_laptops(){
         String rememberFirstElementName = testFacade.getFirstFoundedLaptopName();
         testFacade.findLaptopInMarketByName(rememberFirstElementName);
         String foundLaptopBySearchingField = testFacade.getFirstFoundedLaptopName();
 
-        collector.checkThat("Checking global searching works fine for laptops.",
+        collector.checkThat("Checking global searching works fine for laptops -> AR: "+foundLaptopBySearchingField,
                 rememberFirstElementName,
                 equalTo(foundLaptopBySearchingField));
     }
